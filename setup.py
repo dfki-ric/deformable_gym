f6566fb61baa74fcd612d2dcf1450129a8468453
Melvin Laux <melvin.laux@dfki.de>
2023-08-25 19:38:02 +0200

    update required packages and bump version

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
         long_description = f.read()
 
     setup(name='deformable_gym',
-          version="0.1.1",
+          version="0.2.0",
           maintainer='Melvin Laux',
-          maintainer_email='melvin.laux@dfki.de',
-          description='APRIL gym environments for grasping deformable objects',
+          maintainer_email='melvin.laux@uni-bremen.de',
+          description='Gym environments for grasping deformable objects',
           long_description=long_description,
           long_description_content_type="text/markdown",
           license='BSD-3-Clause',
           packages=["deformable_gym"],
           install_requires=["pybullet",
-                            "gym",
+                            "gymnasium",
                             "numpy",
                             "pytransform3d"
                             ])

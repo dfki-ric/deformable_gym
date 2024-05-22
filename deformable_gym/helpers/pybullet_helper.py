23c055ef1484780653c6fe4bf3d89ec1023c790b
Melvin Laux <melvin.laux@gmail.com>
2024-05-22 20:24:54 +0200

    redirect stdout helper function

--- deformable_gym/helpers/pybullet_helper.py
+++ deformable_gym/helpers/pybullet_helper.py
@@ -12,6 +12,30 @@ import pytransform3d.rotations as pr
 
 from pybullet_utils import bullet_client as bc
 
+from contextlib import contextmanager
+import sys
+import os
+
+
+@contextmanager
+def stdout_redirected(to=os.devnull):
+
+    fd = sys.stdout.fileno()
+
+    def _redirect_stdout(to):
+        sys.stdout.close()
+        os.dup2(to.fileno(), fd)
+        sys.stdout = os.fdopen(fd, 'w')
+
+    with os.fdopen(os.dup(fd), 'w') as old_stdout:
+        with open(to, 'w') as file:
+            _redirect_stdout(to=file)
+        try:
+            yield
+        finally:
+            _redirect_stdout(to=old_stdout)
+
+
 
 class JointType(Enum):
     revolute = pb.JOINT_REVOLUTE

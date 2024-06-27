42e16e20a71af424fbc36281888a8c6f1f6db7e8
Melvin Laux <melvin.laux@gmail.com>
2024-06-27 21:43:22 +0200

    Fix typos

--- deformable_gym/__init__.py
+++ deformable_gym/__init__.py
@@ -10,10 +10,64 @@ register(
 )
 
 register(
+    id="FloatingMiaGraspPillow-v0",
+    entry_point="deformable_gym.envs.floating_mia_grasp_env:FloatingMiaGraspEnv",
+    kwargs={
+        "object_name": "pillow_small",
+        "observable_object_pos": True,
+    },
+)
+
+register(
     id="FloatingShadowGraspInsole-v0",
     entry_point="deformable_gym.envs.floating_shadow_grasp_env:FloatingShadowGraspEnv",
     kwargs={
         "object_name": "insole_on_conveyor_belt/back",
+        "observable_object_pos": True,
+    },
+)
+
+register(
+    id="FloatingShadowGraspPillow-v0",
+    entry_point="deformable_gym.envs.floating_shadow_grasp_env:FloatingShadowGraspEnv",
+    kwargs={
+        "object_name": "pillow_small",
+        "observable_object_pos": True,
+    },
+)
+
+register(
+    id="URMiaGraspInsole-v0",
+    entry_point="deformable_gym.envs.ur5_mia_grasp_env:UR5MiaGraspEnv",
+    kwargs={
+        "object_name": "insole_on_conveyor_belt/back",
+        "observable_object_pos": True,
+    },
+)
+
+register(
+    id="URMiaGraspPillow-v0",
+    entry_point="deformable_gym.envs.ur5_mia_grasp_env:UR5MiaGraspEnv",
+    kwargs={
+        "object_name": "pillow_small",
+        "observable_object_pos": True,
+    },
+)
+
+register(
+    id="URShadowGraspInsole-v0",
+    entry_point="deformable_gym.envs.ur10_shadow_grasp_env:UR10ShadowGraspEnv",
+    kwargs={
+        "object_name": "insole_on_conveyor_belt/back",
+        "observable_object_pos": True,
+    },
+)
+
+register(
+    id="URShadowGraspPillow-v0",
+    entry_point="deformable_gym.envs.ur10_shadow_grasp_env:UR10ShadowGraspEnv",
+    kwargs={
+        "object_name": "pillow_small",
         "observable_object_pos": True,
     },
 )

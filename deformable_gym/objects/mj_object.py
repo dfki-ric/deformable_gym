00e859ac8605c3372750ea617f6b314639d0f278
Kaixing Xiao <xkx971128@gmail.com>
2024-07-22 18:55:26 +0200

    add set position to set initial position

--- deformable_gym/objects/mj_object.py
+++ deformable_gym/objects/mj_object.py
@@ -1,41 +1,42 @@
 import mujoco
-from numpy.typing import NDArray
-
-from ..helpers import mj_utils as mju
+import numpy as np
+from numpy.typing import ArrayLike, NDArray
 
 
 class MJObject:
 
-    def __init__(self, xml_path) -> None:
+    def __init__(self, model: mujoco.MjModel, name: str) -> None:
+        self.model = model
+        self.name = name
 
-        self.model, self.data = mju.load_model(xml_path)
+    def set_position(
+        self,
+        model: mujoco.MjModel,
+        data: mujoco.MjData,
+        pos: ArrayLike,
+    ) -> None:
+        assert len(pos) == 3, f"Position should be a 3D vector, now it is {pos}"
+        model.body(self.name).pos[:] = pos
+        mujoco.mj_forward(model, data)
 
-    @property
-    def name(self) -> str:
-        names = mju.get_body_names(self.model)
-        assert (
-            len(names) == 1
-        ), f"Only one 1st level body is defined in object file, now it is {names}"
-        return names[0]
-
-    def get_com(self, data: mujoco.MjData):
+    def get_com(self, data: mujoco.MjData) -> NDArray:
         return data.body(self.name).ipos
 
 
 class ObjectFactory:
 
     @staticmethod
-    def create(name: str, xml_path: str) -> MJObject:
-        if name == "insole":
-            return Insole(xml_path)
+    def create(model: mujoco.MjModel, name: str) -> MJObject:
+        if name == "insole_fixed":
+            return InsoleFixed(model)
         else:
             raise ValueError(f"Object {name} not found")
 
 
-class Insole(MJObject):
+class InsoleFixed(MJObject):
 
-    def __init__(self, xml_path) -> None:
-        super().__init__(xml_path)
+    def __init__(self, model: mujoco.MjModel) -> None:
+        super().__init__(model, "insole_fixed")
 
     def get_com(self, data: mujoco.MjData) -> NDArray:
         return data.body(self.name).subtree_com
--- deformable_gym/robots/mj_robot.py
+++ deformable_gym/robots/mj_robot.py
@@ -1,32 +1,30 @@
-from typing import Tuple
-
 import mujoco
-import numpy as np
-from numpy.typing import NDArray
-
-from ..helpers import mj_utils as mju
+from numpy.typing import ArrayLike, NDArray
 
 
 class MJRobot:
 
-    def __init__(self, model_path: str, init_frame: str = None):
+    def __init__(self, model: mujoco.MjModel, name: str) -> None:
 
-        self.model, self.data = mju.load_model(model_path)
+        self.model = model
+        self.name = name
         self.nq = self.model.nq
         self.dof = self.model.nv
         self.nact = self.model.nu
         self.ctrl_range = self.model.actuator_ctrlrange.copy()
-
-    @property
-    def name(self) -> str:
-        names = mju.get_body_names(self.model)
-        assert (
-            len(names) == 1
-        ), f"Only one 1st level body is defined in object file, now it is {names}"
-        return names[0]
 
     def get_qpos(self, data: mujoco.MjData) -> NDArray:
         return data.qpos[: self.nq].copy()
 
     def get_qvel(self, data: mujoco.MjData) -> NDArray:
         return data.qvel[: self.dof].copy()
+
+    def set_position(
+        self,
+        model: mujoco.MjModel,
+        data: mujoco.MjData,
+        pos: ArrayLike,
+    ) -> None:
+        assert len(pos) == 3, f"Position should be a 3D vector, now it is {pos}"
+        model.body(self.name).pos[:] = pos
+        mujoco.mj_forward(model, data)

afca959d3e5dfe1cb679cf7074189500b93328ae
Kaixing Xiao <xkx971128@gmail.com>
2024-07-23 18:34:37 +0200

    Add equality constraint

--- deformable_gym/objects/mj_object.py
+++ deformable_gym/objects/mj_object.py
@@ -1,13 +1,30 @@
+from typing import List, Union
+
 import mujoco
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
 
+from ..envs.mujoco.asset_manager import AssetManager
+from ..helpers import mj_utils as mju
+
 
 class MJObject:
 
-    def __init__(self, model: mujoco.MjModel, name: str) -> None:
-        self.model = model
+    def __init__(self, name: str) -> None:
         self.name = name
+        self.model = self._load_model(name)
+
+    @property
+    def eq_constraints(self) -> List[str]:
+        return mju.get_equality_names(self.model)
+
+    @property
+    def eq_constraints_to_disable(self) -> List[Union[str, None]]:
+        return []
+
+    def _load_model(self, name: str) -> mujoco.MjModel:
+        manager = AssetManager()
+        return manager.load_asset(name)
 
     def set_position(
         self,
@@ -19,24 +36,35 @@ class MJObject:
         model.body(self.name).pos[:] = pos
         mujoco.mj_forward(model, data)
 
-    def get_com(self, data: mujoco.MjData) -> NDArray:
+    def get_current_com(self, data: mujoco.MjData) -> NDArray:
         return data.body(self.name).ipos
+
+    def disable_eq_constraint(
+        self, model: mujoco.MjModel, data: mujoco.MjData, *name: str
+    ) -> None:
+        if len(name) == 0:
+            name = self.eq_constraints_to_disable
+        mju.disable_equality_constraint(model, data, *name)
+
+
+class InsoleFixed(MJObject):
+
+    def __init__(self) -> None:
+        super().__init__("insole_fixed")
+
+    @property
+    def eq_constraints_to_disable(self) -> List[str]:
+        return self.eq_constraints
+
+    def get_current_com(self, data: mujoco.MjData) -> NDArray:
+        return data.body(self.name).subtree_com
 
 
 class ObjectFactory:
 
     @staticmethod
-    def create(model: mujoco.MjModel, name: str) -> MJObject:
+    def create(name: str) -> MJObject:
         if name == "insole_fixed":
-            return InsoleFixed(model)
+            return InsoleFixed()
         else:
             raise ValueError(f"Object {name} not found")
-
-
-class InsoleFixed(MJObject):
-
-    def __init__(self, model: mujoco.MjModel) -> None:
-        super().__init__(model, "insole_fixed")
-
-    def get_com(self, data: mujoco.MjData) -> NDArray:
-        return data.body(self.name).subtree_com
